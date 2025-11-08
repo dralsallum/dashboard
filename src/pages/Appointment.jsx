d66061ecf14000122ca27644ab643dfce08909fa
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2025-11-08 16:05:53 +0300

    hello

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -2,16 +2,9 @@ import React, { useEffect } from "react";
 import { Website } from "../components";
 
 const Appointment = () => {
-  const [trigger, setTrigger] = useState(0);
-
   useEffect(() => {
     window.scrollTo(0, 0);
-
-    // Trigger a second run
-    if (trigger === 0) {
-      setTrigger(1);
-    }
-  }, [trigger]);
+  }, []);
   return (
     <>
       <Website />
--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -2,9 +2,16 @@ import React, { useEffect } from "react";
 import { Footer, NavTech, Times } from "../components";
 
 const Reservation = () => {
+  const [trigger, setTrigger] = useState(0);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    // Trigger a second run
+    if (trigger === 0) {
+      setTrigger(1);
+    }
+  }, [trigger]);
   return (
     <>
       <NavTech />
