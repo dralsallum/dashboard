5413882dbb82f5e4977b58120b638f426c3c6feb
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2025-11-08 15:50:47 +0300

    hello

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { NavTech, SignLog } from "../components";
 
 const Login = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <NavTech />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavTech, SignForm } from "../components";
 const SignUp = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <NavTech />
