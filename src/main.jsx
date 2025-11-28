bd392f7a6269a15424cd96627b75f0343466bfce
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2025-11-28 10:21:09 +0300

    jdsakl

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { analytics } from "./firebase";
+import { logEvent } from "firebase/analytics";
+
+logEvent(analytics, "page_view", { page_title: "Home Page" });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
